77025c290016debf7efe6506fcc7f004e5b58e16
tisou1 <57515396+tisou1@users.noreply.github.com>
2023-12-22 12:32:38 +0800

    chore: done

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,58 @@
 
 use std::error::Error;
-use std::fs;
+use std::{fs, env};
 
  pub struct Config {
   query: String,
-  file_path: String,
+  file_path: String, 
+  pub ignore_case: bool, // 是否要开启大小写敏感
 }
 
 impl Config {
-  pub fn build(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          // panic!("not enough arguments");
-          return Err("not enough arguments");
-      }
-      let query = args[1].clone();
-      let file_path = args[2].clone();
-     Ok( Config{query, file_path})
+  pub fn build(
+    mut args: impl Iterator<Item = String>,
+  ) -> Result<Config, &'static str> {
+      // if args.len() < 3 {
+      //     // panic!("not enough arguments");
+      //     return Err("not enough arguments");
+      // }
+      args.next();
+
+      let query = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a query string"),
+      };
+      let file_path = match args.next() {
+          Some(arg) => arg,
+          None => return  Err("Didn't get a file path"),
+      };
+
+      // var读取环境变量, is_ok是Result提供的, 有值返回true没值返回false
+      // let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+      // map_or_else 是Result上的方法, 接受两个闭包参数, 第一个处理Err情况 第二个处理Ok情况
+      let ignore_case =  env::var("IGNORE_CASE").map_or_else(|_| {
+        args
+          .any(|arg| arg.to_lowercase() == "-i" || arg.to_lowercase() == "--ignore-case")
+      },
+      |env_value| env_value == "0" || env_value.to_lowercase() == "false",
+      );
+     Ok( Config{query, file_path, ignore_case})
   }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents =  fs::read_to_string(config.file_path)?;
   
-  for line in search(&config.query, &contents) {
-    println!("{line}")
+
+  let results = if config.ignore_case {
+    search_case_insensitive(&config.query, &contents)
+  } else {
+    search(&config.query, &contents)
+  };
+
+  for line in results {
+    println!("{line}");
   }
 
   Ok(())
@@ -31,17 +60,43 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
- let mut results = Vec::new();
- for line in contents.lines() {
-  if line.contains(query) {
-    results.push(line.trim());
-  }
- }
- results
+//  let mut results = Vec::new();
+//  for line in contents.lines() {
+//   if line.contains(query) {
+//     results.push(line.trim());
+//   }
+//  }
+//  results
+
+contents
+  .lines()
+  .filter(|line| line.contains(query))
+  .collect()
+}
+
+pub fn search_case_insensitive<'a>(
+  query: &str,
+  contents: &'a str
+) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  // let mut results = Vec::new();
+
+  // for line in contents.lines() {
+  //   if line.to_lowercase().contains(&query) {
+  //     results.push(line);
+  //   }
+  // }
+  // results
+
+  contents
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&query))
+    .collect()
 }
 
 #[cfg(test)]
 mod tests {
+  // 导入付模块的所有公共项
   use super::*;
 
   // 注定失败的
@@ -55,4 +110,19 @@ mod tests {
 
     assert_eq!(vec!["Safe, fast, productive"], search(query, contents));
   }
+
+  #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,21 @@ use minigrep::Config;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
     // 读取的第一个参数数为可执行路径名, 后面才是我们输入的
+    // println!("success");
 
-    // 存储读取到的参数
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    // 存储读取到的参数, 直接穿给build迭代器
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
+
     // 文件读取
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
    
